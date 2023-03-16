a23340ea20cb76fe340b874a7f47ff7f869fd38d
RPDavlyatshin <romadavly@gmail.com>
2023-03-16 12:30:08 +0500

    Change requirenments.txt

--- server.py
+++ server.py
@@ -15,6 +15,11 @@ def yes():
     return 'Скажи да'
 
 
+@app.route('/no')
+def no():
+    return 'None'
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 3000))
     app.run(host='0.0.0.0', port=port)
